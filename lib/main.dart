b3a453d935affb4d2e08e331bb32a14829053209
Andre Cunha <andre.freitas.cunha@gmail.com>
2026-02-12 10:20:13 -0300

    colocando primeiro botão

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(new PerguntaApp());
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Perguntas')),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+          ],
+        ),
       ),
     );
   }
