6ac1124d85698a2affa6dc3d867d3affc18660bc
Andre Cunha <andre.freitas.cunha@gmail.com>
2026-02-12 11:16:49 -0300

    teste3

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  void responder() {
+    print('Pergunta respondida');
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Criando lista de perguntas
     final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
       home: Scaffold(
+        // Título da app
         appBar: AppBar(title: Text('Perguntas')),
         body: Column(
           children: [
             Text(perguntas[0]),
-            ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 3')),
           ],
         ),
       ),
